f6f557431dd134b9ba4c2690abaf0127b67d72b1
luz <lezojeda@gmail.com>
2024-01-02 14:04:39 -0300

    feat: add Ingredient and Recipe models

--- Models/Ingredient.cs
+++ Models/Ingredient.cs
@@ -1,8 +1,10 @@
 namespace CookApi.Models;
 
-public class Ingredients
+public class Ingredient
 {
     public Guid Id { get; set; }
+
     public string Name { get; set; }
+
     public List<string> Substitutions { get; set; }
 }
--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace CookApi.Models;
 
 public enum RecipeDifficulty
@@ -13,20 +11,23 @@ public class Recipe
 {
     public Guid Id { get; set; }
 
-    [MaxLength(150)]
     public string Name { get; set; }
 
-    [MaxLength(50)]
     public List<string> Ingredients { get; set; }
+
     public string Instructions { get; set; }
 
     public List<string> Pictures { get; set; } = new List<string>(); // Default to an empty list
 
-    [StringLength(200)]
     public List<string> Videos { get; set; } = new List<string>();   // Default to an empty list
+
     public int PreparationTime { get; set; }
+
     public int CookingTime { get; set; }
+
     public int Servings { get; set; }
+
     public RecipeDifficulty Difficulty { get; set; }
-    public bool Vegetarian { get; set; } = false;
+
+    public bool Vegetarian { get; set; }
 }
