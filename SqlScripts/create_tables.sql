03e024b040dabb1e7123e816541323d2706d20c9
Luz <luzojeda@proton.me>
2024-04-01 12:38:15 -0300

    Update create_tables.sql

--- SqlScripts/create_tables.sql
+++ SqlScripts/create_tables.sql
@@ -1,55 +1,71 @@
-CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
-    "MigrationId" character varying(150) NOT NULL,
-    "ProductVersion" character varying(32) NOT NULL,
-    CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
-);
-
-START TRANSACTION;
-
-CREATE TABLE "Ingredients" (
+CREATE TABLE public."Ingredients" (
     "Id" uuid NOT NULL,
     "Name" character varying(50) NOT NULL,
-    "Substitutions" varchar(50)[] NOT NULL,
-    CONSTRAINT "PK_Ingredients" PRIMARY KEY ("Id")
+    "Substitutions" character varying(50)[] NOT NULL
 );
 
-CREATE TABLE "Recipes" (
+
+ALTER TABLE public."Ingredients" OWNER TO postgres;
+
+CREATE TABLE public."Recipes" (
     "Id" uuid NOT NULL,
     "Name" character varying(150) NOT NULL,
-    "Ingredients" varchar(200)[] NOT NULL,
+    "Ingredients" character varying(200)[] NOT NULL,
     "Instructions" text NOT NULL,
-    "Pictures" varchar(200)[] NOT NULL DEFAULT ARRAY[]::varchar(200)[],
-    "Videos" varchar(200)[] NOT NULL DEFAULT ARRAY[]::varchar(200)[],
-    "PreparationTime" integer NULL,
-    "CookingTime" integer NULL,
-    "Servings" integer NULL,
-    "Difficulty" character varying(6) NULL,
-    "Vegetarian" boolean NOT NULL DEFAULT FALSE,
-    CONSTRAINT "PK_Recipes" PRIMARY KEY ("Id"),
-    CONSTRAINT "CK_Recipe_CookingTime" CHECK ("CookingTime" >= 0 OR "PreparationTime" IS NULL),
-    CONSTRAINT "CK_Recipe_Difficulty" CHECK ("Difficulty" IN ('Easy', 'Medium', 'Hard') OR "Difficulty" IS NULL),
-    CONSTRAINT "CK_Recipe_Ingredients" CHECK (cardinality("Ingredients") > 0),
-    CONSTRAINT "CK_Recipe_PreparationTime" CHECK ("PreparationTime" > 0 OR "PreparationTime" IS NULL)
+    "Pictures" character varying(200)[] DEFAULT (ARRAY[]::character varying[])::character varying(200)[] NOT NULL,
+    "Videos" character varying(200)[] DEFAULT (ARRAY[]::character varying[])::character varying(200)[] NOT NULL,
+    "PreparationTime" integer,
+    "CookingTime" integer,
+    "Servings" integer,
+    "Difficulty" character varying(6),
+    "Vegetarian" boolean DEFAULT false NOT NULL,
+    "Summary" character varying(150),
+    CONSTRAINT "CK_Recipe_CookingTime" CHECK ((("CookingTime" >= 0) OR ("PreparationTime" IS NULL))),
+    CONSTRAINT "CK_Recipe_Difficulty" CHECK (((("Difficulty")::text = ANY ((ARRAY['Easy'::character varying, 'Medium'::character varying, 'Hard'::character varying])::text[])) OR ("Difficulty" IS NULL))),
+    CONSTRAINT "CK_Recipe_Ingredients" CHECK ((cardinality("Ingredients") > 0)),
+    CONSTRAINT "CK_Recipe_PreparationTime" CHECK ((("PreparationTime" > 0) OR ("PreparationTime" IS NULL)))
 );
 
-CREATE UNIQUE INDEX "IX_Ingredients_Name" ON "Ingredients" ("Name");
 
-CREATE INDEX "IX_Recipes_Ingredients" ON "Recipes" USING gin ("Ingredients");
+ALTER TABLE public."Recipes" OWNER TO postgres;
 
-CREATE UNIQUE INDEX "IX_Recipes_Name" ON "Recipes" ("Name");
+--
+-- TOC entry 3186 (class 2606 OID 17561)
+-- Name: Ingredients PK_Ingredients; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
 
-INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
-VALUES ('20240123225333_InitialMigrate', '7.0.11');
+ALTER TABLE ONLY public."Ingredients"
+    ADD CONSTRAINT "PK_Ingredients" PRIMARY KEY ("Id");
 
-COMMIT;
 
-START TRANSACTION;
+--
+-- TOC entry 3190 (class 2606 OID 17575)
+-- Name: Recipes PK_Recipes; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
 
-ALTER TABLE "Recipes" ALTER COLUMN "Videos" SET DEFAULT ARRAY[]::varchar(200)[];
+ALTER TABLE ONLY public."Recipes"
+    ADD CONSTRAINT "PK_Recipes" PRIMARY KEY ("Id");
 
-ALTER TABLE "Recipes" ALTER COLUMN "Pictures" SET DEFAULT ARRAY[]::varchar(200)[];
 
-INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
-VALUES ('20240130185601_InitialCreate', '7.0.11');
+--
+-- TOC entry 3184 (class 1259 OID 17576)
+-- Name: IX_Ingredients_Name; Type: INDEX; Schema: public; Owner: postgres
+--
 
-COMMIT;
+CREATE UNIQUE INDEX "IX_Ingredients_Name" ON public."Ingredients" USING btree ("Name");
+
+
+--
+-- TOC entry 3187 (class 1259 OID 17577)
+-- Name: IX_Recipes_Ingredients; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX "IX_Recipes_Ingredients" ON public."Recipes" USING gin ("Ingredients");
+
+
+--
+-- TOC entry 3188 (class 1259 OID 17578)
+-- Name: IX_Recipes_Name; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX "IX_Recipes_Name" ON public."Recipes" USING btree ("Name");
