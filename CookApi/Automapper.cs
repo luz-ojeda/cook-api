a84a9475d5f812a83a71530a60eee96ac76b50ab
Luz <luzojeda@proton.me>
2024-04-23 23:00:35 -0300

    Add PATCH endpoint

--- CookApi/Automapper.cs
+++ CookApi/Automapper.cs
@@ -7,7 +7,19 @@ public class AutoMapperProfile : Profile
     public AutoMapperProfile()
     {
         CreateMap<Recipe, RecipeDTO>();
-        CreateMap<RecipeDTO, Recipe>()
+        CreateMap<UpdateRecipeDTO, Recipe>()
+			.ForMember(dest => dest.Name, opt => opt.Condition(src => src.Name != null))
+			.ForMember(dest => dest.Summary, opt => opt.Condition(src => src.Summary != null))
+			.ForMember(dest => dest.Ingredients, opt => opt.Condition(src => src.Ingredients != null))
+			.ForMember(dest => dest.Instructions, opt => opt.Condition(src => src.Instructions != null))
+			.ForMember(dest => dest.Pictures, opt => opt.Condition(src => src.Pictures != null))
+			.ForMember(dest => dest.Videos, opt => opt.Condition(src => src.Videos != null))
+			.ForMember(dest => dest.PreparationTime, opt => opt.Condition(src => src.PreparationTime != null))
+			.ForMember(dest => dest.CookingTime, opt => opt.Condition(src => src.CookingTime != null))
+			.ForMember(dest => dest.Servings, opt => opt.Condition(src => src.Servings != null))
+			.ForMember(dest => dest.Difficulty, opt => opt.Condition(src => src.Difficulty != null))
+			.ForMember(dest => dest.Vegetarian, opt => opt.MapFrom(src => src.Vegetarian));
+		CreateMap<RecipeDTO, Recipe>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid()))
             .ForMember(dest => dest.Ingredients, opt => opt.MapFrom(src => MapIngredients(src.Ingredients)))
             .ForMember(dest => dest.Difficulty, opt => opt.MapFrom(src => MapDifficulty(src.Difficulty)));
